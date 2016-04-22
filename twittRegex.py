fbe52537b4ea16204e3f5be76c5efa28d040ddc5
gabyhg <gabyhernandezgarcia3@gmail.com>
2016-04-22 00:22:31 -0500

    Added files via upload
    Programa usuarios.

---
+++ twittRegex.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  twittRegex.py
+#
+#  The MIT License (MIT)
+#
+#  Copyright (c) 2016
+#    Emilio Cabrera <emilio1625@gmail.com>
+#    Gabriela Hernández García <gabyhernandezgarcia3@gmail.com>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", help="Archivo html de twitter.com del que se extraerán los usuarios")
+    args = parser.parse_args()
+    if args.file:
+        with open(args.file, "r") as archivo:
+            usuarios = getUsuarios(archivo)
+            print sorted(usuarios)
+
+    else:
+        print "Debes dar un archivo para trabajar"
+        print "Prueba 'twittRegex -h' para más información"
+    return 0
+
+def getUsuarios(archivo):
+    contenido = archivo.read()
+    objRegex = re.findall(r"@(\w+)", contenido)
+    usuarios = []
+    for n in range(0, len(objRegex)):
+        usuarios.append(objRegex[n])
+    objRegex = re.findall(r"@<\/s><(\w+)>(.*?)<\/(\1)>", contenido)
+    for n in range(0, len(objRegex)):
+        a = objRegex[n][1]
+        if (a != ''):
+            usuarios.append(a)
+    objRegex = re.findall(r"@<(.*) (.*)>(.*?)<\/(\1)>", archivo.read())
+    for n in range(0, len(objRegex)):
+        a = objRegex[n][2]
+        if (a != ''):
+            usuarios.append(a)
+    return usuarios
+
+if __name__ == '__main__':
+    import sys
+    import re
+    import argparse
+    sys.exit(main(sys.argv))
